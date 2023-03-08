1d209181c497c74a414ed7c9227402fb59b5135a
DesertStrom16 <deckerstrom@ymail.com>
2023-03-07 19:15:10 -0800

    video sidebar beginnings

--- src/components/VideoScreen.tsx
+++ src/components/VideoScreen.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Flex } from "@mantine/core";
+import { Box, Button, Flex, Text } from "@mantine/core";
 import { useParams } from "react-router-dom";
 import ReactPlayer from "react-player/youtube";
 import { useGetRecommendedQuery } from "../services/watch";
@@ -13,25 +13,88 @@ export default function VideoScreen({}: Props): JSX.Element {
     skip: !id,
   });
 
-  console.log(data)
+  console.log(data);
 
   return (
-    <Flex ml={24} pt={24} pr={24} w="100%">
-      <Flex pos="relative" miw={320} pt="56.25%" w="100%" bg="red">
-        <ReactPlayer
-          width="100%"
-          height="100%"
-          controls={true}
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
+    <Flex
+      w="100%"
+      // maw='100vw'
+      sx={{
+        flexDirection: "column",
+
+        "@media (min-width: 1015px)": {
+          flexDirection: "row",
+        },
+      }}
+    >
+      <Flex ml={24} pt={24} pr={24} w="100%">
+        <Flex
+          pos="relative"
+          pt="56.25%"
+          w="100%"
+          bg="red"
+          sx={{
+            minWidth: "calc(240px * (16 / 9))",
+            maxWidth: "calc((100vh - (56px + 24px + 36px)) * (16/9))",
+
+            "@media (min-width: 1015px)": {
+              minWidth: "calc(360px * (16 / 9))",
+            },
+            "@media (min-width: 1343px)": {
+              minWidth: "calc(480px * (16 / 9))",
+            },
           }}
-          url={`https://www.youtube.com/watch?v=${id}`}
-        />
+        >
+          <ReactPlayer
+            width="100%"
+            height="100%"
+            controls={true}
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+            }}
+            url={`https://www.youtube.com/watch?v=${id}`}
+          />
+        </Flex>
       </Flex>
+
+      <Box
+        sx={{
+          "@media (min-width: 1015px)": {
+            minWidth: 300,
+            maxWidth: 402,
+            width: "100%",
+            paddingTop: 24,
+            paddingRight: 24,
+          },
+        }}
+      >
+        <Flex mb={8} w="100%">
+          <Box
+            h={94}
+            w={168}
+            miw={168}
+            mr={8}
+            sx={{ position: "relative" }}
+          ></Box>
+          <Flex w="100%" pr={24}>
+            <Text
+              color="#f1f1f1"
+              size={14}
+              lh="20px"
+              mah={40}
+              lineClamp={2}
+              fw={500}
+              sx={{ textOverflow: "ellipsis" }}
+            >
+              Sound City
+            </Text>
+          </Flex>
+        </Flex>
+      </Box>
     </Flex>
   );
 }
