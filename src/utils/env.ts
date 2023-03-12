cdb715776d0f1ee6880d7e01409f48d9bae25c3b
DesertStrom16 <deckerstrom@ymail.com>
2023-03-11 18:02:55 -0800

    prod server url

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,7 +1,7 @@
 export const serverUrl =
   process.env.NODE_ENV === "development"
     ? 'http://localhost:8000/'
-    : '';
+    : 'https://blackbird.quest/';
 
 const disableImages = false;
 export const areImagesDisabled =
