47187331e69af843212b39748fe83dbe497e0ed6
Decker Strom <deckerstrom16@gmail.com>
2024-09-10 17:08:43 -0700

    subs through localstorage working

--- src/components/video-screen/ChannelAndSubComp.tsx
+++ src/components/video-screen/ChannelAndSubComp.tsx
@@ -4,14 +4,17 @@ import { useParams } from "react-router-dom";
 import { useGetRecommendedQuery } from "../../services/watch";
 import { useEffect, useState } from "react";
 import { getObject, setObject } from "../../utils/localStorage";
-import { useAppSelector } from "../../app/hooks";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import { addNewSub, removeSub } from "../../features/storage";
 
 type Props = {};
 
 export default function ChannelAndSubComp({}: Props) {
-  const storageSelector = useAppSelector(state => state.storage.subscriptions)
+  const dispatch = useAppDispatch();
+  const storageSubsSelector = useAppSelector(
+    (state) => state.storage.subscriptions
+  );
   const { id } = useParams();
-  // const [subs, setSubs] = useState([]);
   const [imageLoaded, setImageLoaded] = useState(false);
 
   // @ts-expect-error
@@ -19,37 +22,32 @@ export default function ChannelAndSubComp({}: Props) {
     skip: !id,
   });
 
-  const isSubbed = storageSelector.find((item) => item === data?.channelCannonicalURL)
+  const isSubbed = storageSubsSelector.find(
+    (item) => item === data?.channelCannonicalURL
+  );
 
   const imageLoadHandler = () => {
     setImageLoaded(true);
   };
 
-  //   const getSomething = async () => {
-  //     await getObject("@SUBS").then(async (subs) => {
-  //         console.log(subs)
-  //     });
-  //   };
-
-  //   const setSomething = () => {
-  //     setObject("@SUBS", ["@PotentialHistory", "@TheStoryOutWest", "@ShroudedHand"])
-  //   }
-
   const subscribeHandler = () => {
-    // if (subs.length > 0) {
-    //   const isDuplicate = subs.filter(
-    //     (item) => item === data?.channelCannonicalURL
-    //   );
+    //  Button is disabled if "data" is undefined. Otherwise I wouldn't use non-null assertions.
+    if (storageSubsSelector.length > 0) {
+      // Not happy with this at all.
+      setObject("@SUBS", [...storageSubsSelector, data!.channelCannonicalURL]);
+      dispatch(addNewSub(data!.channelCannonicalURL));
+    } else {
+      setObject("@SUBS", [data!.channelCannonicalURL]);
+      dispatch(addNewSub(data!.channelCannonicalURL));
+    }
+  };
 
-    //   console.log(isDuplicate);
-
-    //   if (isDuplicate.length === 0) {
-    //     setObject("@SUBS", [...subs, data?.channelCannonicalURL]);
-    //     console.log("hi");
-    //   }
-    // } else {
-    //   setObject("@SUBS", [data?.channelCannonicalURL]);
-    // }
+  const unsubscribeHandler = () => {
+    let updatedSubs = storageSubsSelector.filter(
+      (item) => item !== data!.channelCannonicalURL
+    );
+    setObject("@SUBS", [...updatedSubs]);
+    dispatch(removeSub(data!.channelCannonicalURL))
   };
 
   return (
@@ -135,7 +133,8 @@ export default function ChannelAndSubComp({}: Props) {
 
       {/* Subscribe Button Container */}
       <button
-        onClick={subscribeHandler}
+        onClick={isSubbed ? unsubscribeHandler : subscribeHandler}
+        disabled={!data}
         id="subscribe-btn"
         style={{
           display: "flex",
@@ -160,7 +159,7 @@ export default function ChannelAndSubComp({}: Props) {
             color: "rgb(241,241,241)",
           }}
         >
-          {isSubbed ? "Subscribed!" : "Subscribe"}
+          {data ? (isSubbed ? "Subscribed!" : "Subscribe") : "Error"}
         </Text>
       </button>
     </Flex>
